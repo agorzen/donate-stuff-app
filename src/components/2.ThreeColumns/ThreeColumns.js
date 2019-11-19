6c054fdd599103597acddc10cadfd5a6fca3b641
Aga Gorzeń <gorzenaga@gmail.com>
2019-11-19 16:49:54 +0100

    basic-threecolumn-component

--- src/components/2.ThreeColumns/ThreeColumns.js
+++ src/components/2.ThreeColumns/ThreeColumns.js
@@ -1,8 +1,80 @@
 import React from "react";
-// import styled, { css } from "styled-components";
 
-function ThreeColumns() {
-  return <></>;
+import styled from "styled-components";
+// function ThreeColumns() {
+//   return <></>;
+// }
+
+const Space = styled.div`
+  width: 100%;
+  height: 384px;
+  background: #fad648;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  justify-content: space-around;
+  padding-right: 142px;
+  margin: 0 auto;
+`;
+const Column = styled.div`
+  text-align: center;
+  width: 334px;
+  height: 252px;
+`;
+const Number = styled.div`
+  height: 122px;
+  margin-top: 2px;
+  font-size: 90px;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 0.7);
+  text-align: center;
+  color: #3c3c3c;
+`;
+const Title = styled.div`
+  height: 24px;
+  font-size: 18px;
+  width: 100%;
+  text-transform: uppercase;
+  margin-top: 10px;
+  display: inline-block;
+  text-align: center;
+  color: #3c3c3c;
+`;
+const Description = styled.div`
+  height: 68px;
+  width: 100%;
+  font-size: 16px;
+  margin-top: 10px;
+  letter-spacing: -0.32px;
+  color: #3c3c3c;
+  display: inline-block;
+  text-align: center;
+  line-height: 1.5em;
+`;
+
+function ThreeColumns({ items }) {
+  return (
+    <Space>
+      {items.map((item, index) => (
+        <FormatColumn
+          key={index}
+          number={item.number}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
+    </Space>
+  );
+}
+
+function FormatColumn({ number, title, description }) {
+  return (
+    <Column>
+      <Number>{number}</Number>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Column>
+  );
 }
 
 export default ThreeColumns;
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,33 @@ import JsonSection from "././5.JsonSection/JsonSection";
 import ContactForm from "././6.ContactForm/ContactForm";
 import Footer from "././7.Footer/Footer";
 
+//ThreeColumns - component:
+const items = [
+  {
+    number: "10",
+    title: "oddanych worków",
+    description:
+      "Lorem ipsum dolor sit ament, consectetur adipisc. Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat."
+  },
+  {
+    number: "5",
+    title: "wspartych organizacji",
+    description:
+      "Lorem ipsum dolor sit ament, consectetur adipisc. Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat."
+  },
+  {
+    number: "7",
+    title: "zorganizowanych zbiórek",
+    description:
+      "Lorem ipsum dolor sit ament, consectetur adipisc. Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat."
+  }
+];
+
 function Home() {
   return (
     <>
       <Header />
-      <ThreeColumns />
+      <ThreeColumns items={items} />
       <FourColumns />
       <AboutUs />
       <JsonSection />
