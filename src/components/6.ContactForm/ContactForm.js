78350ca599ae8dc1fc26da38bbaa0d96fbf0223b
Aga Gorzeń <gorzenaga@gmail.com>
2019-11-25 23:06:44 +0100

    footer-component

--- src/components/6.ContactForm/ContactForm.js
+++ src/components/6.ContactForm/ContactForm.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 // import { NavLink, withRouter } from "react-router-dom";
-
+import Footer from "../7.Footer/Footer";
 import { Link } from "react-scroll";
 import formphoto from "../../assets/assets/Background-Contact-Form.jpg";
 import decoration from "../../assets/assets/Decoration.svg";
-import facebook from "../../assets/assets/Facebook.svg";
-import instagram from "../../assets/assets/Instagram.svg";
 
 const options = {
   activeClass: "active",
@@ -30,35 +28,7 @@ const LeftSection = styled.img`
   height: 1026px;
   width: 50%;
 `;
-const Footer = styled.div`
-  width: 50%;
-  display: flex;
-  margin-right: 142px;
-  justify-content: space-between;
-  float: right;
-`;
 
-const Copyrighted = styled.div`
-  font-size: 16px;
-  height: 30px;
-  text-align: center;
-  font-weight: 400;
-  padding-top: 10px;
-`;
-const Social = styled.div`
-  display: flex;
-`;
-const Facebook = styled.div`
-  background: url(${facebook});
-  width: 30px;
-  height: 30px;
-`;
-const Instagram = styled.div`
-  background: url(${instagram});
-  width: 30px;
-  height: 30px;
-  margin-left: 10px;
-`;
 const RightSection = styled.div`
   width: 50%;
   height: 100%;
@@ -181,7 +151,6 @@ class HomeContact extends Component {
     e.preventDefault();
     const errors = [];
     if (this.state.name.length <= 3) {
-      //tu->przynajmniej 3 znaki
       errors.push("Podane imię jest nieprawidłowe");
     }
     if (this.state.email.length <= 6) {
@@ -252,13 +221,7 @@ class HomeContact extends Component {
             </form>
           </Formcontainer>
         </RightSection>
-        <Footer>
-          <Copyrighted>Copyright by CodersLab</Copyrighted>
-          <Social>
-            <Facebook />
-            <Instagram />
-          </Social>
-        </Footer>
+        <Footer />
       </Space>
     );
     const greetings = <h1>Hello {user.name}</h1>;
--- src/components/7.Footer/Footer.js
+++ src/components/7.Footer/Footer.js
@@ -1,8 +1,50 @@
-import React from "react";
-// import styled, { css } from "styled-components";
+import React, { Component } from "react";
+import styled from "styled-components";
 
+import facebook from "../../assets/assets/Facebook.svg";
+import instagram from "../../assets/assets/Instagram.svg";
+
+const FooterComp = styled.div`
+  width: 50%;
+  display: flex;
+  margin-right: 142px;
+  justify-content: space-between;
+  float: right;
+`;
+
+const Copyrighted = styled.div`
+  font-size: 16px;
+  height: 30px;
+  text-align: center;
+  font-weight: 400;
+  padding-top: 10px;
+`;
+const Social = styled.div`
+  display: flex;
+`;
+const Facebook = styled.div`
+  background: url(${facebook});
+  width: 30px;
+  height: 30px;
+`;
+const Instagram = styled.div`
+  background: url(${instagram});
+  width: 30px;
+  height: 30px;
+  margin-left: 10px;
+`;
 function Footer() {
-  return <></>;
+  return (
+    <>
+      <FooterComp>
+        <Copyrighted>Copyright by CodersLab</Copyrighted>
+        <Social>
+          <Facebook />
+          <Instagram />
+        </Social>
+      </FooterComp>
+    </>
+  );
 }
 
 export default Footer;
