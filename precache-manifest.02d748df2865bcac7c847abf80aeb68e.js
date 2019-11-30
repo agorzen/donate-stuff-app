a008130fdc3026f6a9041143a3ad8d61ea1a55db
Aga Gorzeń <gorzenaga@gmail.com>
2019-11-30 20:58:15 +0100

    Updates

---
+++ precache-manifest.02d748df2865bcac7c847abf80aeb68e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed30a63016467252b26983ebdaa4f774",
+    "url": "/donate-stuff-app/index.html"
+  },
+  {
+    "revision": "10839aeed3b4eec21309",
+    "url": "/donate-stuff-app/static/css/main.8fd52f4e.chunk.css"
+  },
+  {
+    "revision": "f0659d51861ddf40bacd",
+    "url": "/donate-stuff-app/static/js/2.5bfc2a0d.chunk.js"
+  },
+  {
+    "revision": "10839aeed3b4eec21309",
+    "url": "/donate-stuff-app/static/js/main.e9c3bfa2.chunk.js"
+  },
+  {
+    "revision": "d0e89cd4feecbdc8e715",
+    "url": "/donate-stuff-app/static/js/runtime-main.18097b0a.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/donate-stuff-app/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "e24b3fd930238dff92b09f3460b9f50a",
+    "url": "/donate-stuff-app/static/media/Decoration.e24b3fd9.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/donate-stuff-app/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/donate-stuff-app/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/donate-stuff-app/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/donate-stuff-app/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/donate-stuff-app/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/donate-stuff-app/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/donate-stuff-app/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/donate-stuff-app/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/donate-stuff-app/static/media/Signature.e54cfcfc.svg"
+  }
+]);
